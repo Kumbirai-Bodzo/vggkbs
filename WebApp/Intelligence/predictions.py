f606b95d2f254e998ed8a393bbfa26231c22dc68
Kumbirai Bodzo <bodzok@gmail.com>
2021-06-22 23:09:05 +0200

    d

--- WebApp/Intelligence/predictions.py
+++ WebApp/Intelligence/predictions.py
@@ -113,6 +113,7 @@ class VggProcess():
             model = VGG16(weights = vgg16_weights)
 
         except Exception as e:
+            print(e)
             model = VGG16(weights='https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
             
 
